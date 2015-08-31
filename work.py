5913fcce3f735dc1804680bfde458263fb3c00d7
hostage112 <ess.aleksandr@gmail.com>
2015-08-31 23:47:47 +0300

    working version

--- work.py
+++ work.py
@@ -37,26 +37,26 @@ def generateWestgaard(New, Old, v):
         njuu = (1 - 2 * v) / (2 * (1 - v))
         soilConst = (2 * math.pi)
         distInfluence = ((r / (njuu * z))** 2 + 1) ** (3. / 2)
-        sigma = P / (soilConst * (njuu ** 2) * (z ** 2) * distInfluence)
+        sigma = P / (soilConst * (njuu) * (z ** 2) * distInfluence)
         calculationSteps += 1
         return sigma
         
-    def calculatepNorm(i):
-        sumpNorm = 0
+    def calculatePnorm(i):
+        sumPnorm = 0
         for j in Old.finits.keys():
             dx = abs(New.nodes[i].x - Old.finits[j].x)
             dy = abs(New.nodes[i].y - Old.finits[j].y)
             r = (dx**2 + dy ** 2) ** (1. / 2)
             P = Old.pNorms[j] * Old.finits[j].area
-            sumpNorm += Westergaard(r, v, dz, P)
-        return sumpNorm
+            sumPnorm += Westergaard(r, v, dz, P)
+        return sumPnorm
         
     dz = abs(New.H - Old.H)
     nodePnorms = {}
 
     for i in New.nodes.keys():
-        newpNorm = calculatepNorm(i)
-        nodePnorms[i] = newpNorm
+        nodePnorm = calculatePnorm(i)
+        nodePnorms[i] = nodePnorm
 
     for k in New.finits.keys():
         finitPnorm = 0
