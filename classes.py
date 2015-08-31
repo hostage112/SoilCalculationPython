f6ce584704dd42e8628ca55e546a009ebdb0c529
hostage112 <ess.aleksandr@gmail.com>
2015-08-31 23:41:23 +0300

    calculation fix

--- classes.py
+++ classes.py
@@ -69,7 +69,7 @@ class FinitElementData(object):
             njuu = (1 - 2 * v) / (2 * (1 - v))
             soilConst = (2 * math.pi)
             distInfluence = ((r / (njuu * z))** 2 + 1) ** (3. / 2)
-            sigma = P / (soilConst * (njuu ** 2) * (z ** 2) * distInfluence)
+            sigma = P / (soilConst * (njuu) * (z ** 2) * distInfluence)
             hhg += 1
             return sigma
         
