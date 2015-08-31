16635c227134a19c4323c6035a76a6d989a4131d
hostage112 <ess.aleksandr@gmail.com>
2015-08-31 23:33:40 +0300

    restructuring

--- main.py
+++ main.py
@@ -1,54 +1,35 @@
 # -*- coding: utf-8 -*-
-import os
 import time
-import imports
+import datetime
 import classes as data
+import work as work
 
-def importFiles():
-    PATH, file = os.path.split(os.path.realpath(__file__))
-    SOIL = "\\soil.txt"
-    NODE = "\\nodes.csv" #"\\realNodes_05.csv" #"\\nodes.csv"
-    FINIT = "\\finitElements.csv" #"\\realFinits_05.csv" #"\\finitElements.csv" 
-    
-    soil = imports.importSoilData(PATH + SOIL)
-    nodes = imports.importNodeData(PATH + NODE)
-    finits = imports.importFinitElements(nodes, PATH + FINIT)
-    
-    print "imports... done"
-    return soil, nodes, finits
-
-def createBaseCase(finits, H):
-    H0 = data.FinitElementData(H)
-    
-    for i in finits.keys():
-        H0.insertName(finits[i].name)
-        H0.insertX(finits[i].x)
-        H0.insertY(finits[i].y)
-        H0.insertArea(finits[i].area)
-        H0.insertCorners(finits[i].corners)
-        H0.insertpNorm(finits[i].pNorm)
-        
-    print "H0 init... done"
-    return H0
-
+#SETTINGS
+Depth = -2.95
+deltaDepth = 3.45
+case = "test" #"test" #"real"
+poisson = 0.35
+#SETTINGS
 
 t1 = time.time()
 
-Depth = -2.95
-deltaDepth = 3.45
+nodes, finits, pNorms = work.importFiles(case)
 
-soil, nodes, finits = importFiles()
-H0 = createBaseCase(finits, Depth)
+print
+print "Arvutus hakkas:", datetime.datetime.now().time()
+print "Arvutus võib võtta ligikaudu:", (len(nodes)*len(finits))/200000/60, "min"
+
+H0 = data.FinitElementData(finits, nodes, Depth)
+work.createBaseCase(H0, pNorms)
 H0.findMaxPnorm()
 
 Depth -= deltaDepth
-H1 = data.FinitElementData(Depth)
-H1.generateBlank(H0)
-H1.generateWestgaard(H0, nodes, soil)
+H1 = data.FinitElementData(finits, nodes, Depth)
+work.generateWestgaard(H1, H0, poisson)
 H1.findMaxPnorm()
     
 t2 = time.time()
 dt = t2 - t1
-print
-print "Arvutuse aeg:", dt/60, "min"
 
+print
+print "Arvutuse võttis aega:", dt/60, "min"
