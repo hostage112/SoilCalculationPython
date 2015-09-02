121dd2b859a2236bbca271ded8dfe834f18b9f25
hostage112 <ess.aleksandr@gmail.com>
2015-09-02 13:59:59 +0300

    new mesh start

--- main.py
+++ main.py
@@ -3,18 +3,20 @@ import time
 import datetime
 import classes as data
 import work as work
+import mesh as mesh
 
 #SETTINGS
 Depth = -2.95
 deltaDepth = 3.45
-case = "real" #"test" #"real"
+case = "test" #"test" #"real"
 poisson = 0.35
 #SETTINGS
 
 t1 = time.time()
 
-nodes, finits, pNorms = work.importFiles(case)
-
+robotNodes, robotFinits, robotPnorms = work.importFiles(case)
+mesh.generateNewMesh(robotNodes, 0.5)
+"""
 print
 print "Arvutus hakkas:", datetime.datetime.now().time()
 print "Arvutus võib võtta ligikaudu:", (len(nodes)*len(finits))/200000/60, "min"
@@ -26,7 +28,7 @@ H0.findMaxPnorm()
 H0.plotSelf("Base")
 
 Depth -= deltaDepth
-"""
+
 H1_west1 = data.FinitElementData(finits, nodes, Depth)
 work.generateWestgaard(H1_west1, H0, 0.0)
 H1_west1.findMaxPnorm()
@@ -42,7 +44,7 @@ H1_west3.findMaxPnorm()
 H1_west4 = data.FinitElementData(finits, nodes, Depth)
 work.generateWestgaard(H1_west4, H0, 0.3)
 H1_west4.findMaxPnorm()
-"""
+
 H1_west5 = data.FinitElementData(finits, nodes, Depth)
 work.generateWestgaard(H1_west5, H0, 0.35)
 H1_west5.findMaxPnorm()
@@ -56,3 +58,4 @@ dt = t2 - t1
 
 print
 print "Arvutuse võttis aega:", dt/60, "min"
+"""
---
+++ mesh.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+import classes as data
+
+def generateNewMesh(robotNodes, finitSize):
+    x0 = 0.0
+    xm = 0.0
+    y0 = 0.0
+    ym = 0.0
+
+    for i in robotNodes.keys():
+        if robotNodes[i].x < x0:
+            x0 = robotNodes[i].x
+        if robotNodes[i].x > xm:
+            xm = robotNodes[i].x
+        if robotNodes[i].y < y0:
+            y0 = robotNodes[i].y
+        if robotNodes[i].y > ym:
+            ym = robotNodes[i].y
+      
+    dx = abs(xm - x0)
+    dy = abs(ym - y0)
+    
+    print      
+    print "old", x0, xm, y0, ym
+    print dx, dy
+        
+    a = 0.1 * dx
+    b = 0.1 * dy
+    
+    modx = (dx + 2*a) % finitSize
+    mody = (dy + 2*b) % finitSize
+    
+    if modx != 0:
+        print "täpstustan"
+        a += modx/2 + finitSize/2
+        modx = (dx + 2*a) % finitSize
+    if mody != 0:
+        b += mody/2 + finitSize/2
+        mody = (dy + 2*b) % finitSize
+    
+    newX0 = x0 - a
+    newY0 = y0 - b
+    newXmax = xm + a
+    newYmax = ym + b
+    
+    dx = abs(newXmax - newX0)
+    dy = abs(newYmax - newY0)
+    
+    print
+    print "new", newX0, newY0, newXmax, newYmax
+    print dx, dy
+    print finitSize
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
