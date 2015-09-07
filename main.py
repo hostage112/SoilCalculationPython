f489d605e38042bdf68b20a4fe0d2eb11f4ce366
hostage112 <ess.aleksandr@gmail.com>
2015-09-07 17:58:39 +0300

    meshing testing

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import mesh as mesh
 case = "test" #"test" #"real"
 
 finitSize = 0.5
-calcAreaMultiplier = 2.0
+calcAreaMultiplier = 1.7
 
 Depth = -2.95
 deltaDepth = 3.45
@@ -22,30 +22,33 @@ t1 = time.time()
 robotNodes, robotFinits, robotPnorms = work.importFiles(case)
 calcNodes, calcFinits = mesh.generateNewMesh(robotNodes, 
                                             finitSize, calcAreaMultiplier)
-print
-print "Arvutus hakkas:", datetime.datetime.now().time()
-print "Arvutus võib võtta ligikaudu:", (len(calcNodes)*len(robotNodes))/200000/60, "min"
-print len(calcNodes)
+
+t2 = time.time()
 
 H0 = data.FinitElementData(robotNodes, robotFinits, Depth)
 work.createBaseCase(H0, robotPnorms)
 H0.findMaxPnorm()
-H0.plotSelf("Base")
+#H0.plotSelf("Base")
 
 Depth -= deltaDepth
 
 H1 = data.FinitElementData(calcNodes, calcFinits, Depth)
 work.generateWestgaard(H1, H0, poisson)
 H1.findMaxPnorm()
-H1.plotSelf("west", + poisson)
+H1.plotSelf("west")
 
-H2 = data.FinitElementData(calcNodes, calcFinits, Depth)
-work.generateBoussinesq(H2, H0)
-H2.findMaxPnorm()
-H2.plotSelf("bouss")
+#H2 = data.FinitElementData(calcNodes, calcFinits, Depth)
+#work.generateBoussinesq(H2, H0)
+#H2.findMaxPnorm()
+#H2.plotSelf("bouss")
 
-t2 = time.time()
-dt = t2 - t1
+
+t3 = time.time()
+dt1 = t2 - t1
+dt2 = t3 - t2
+dtk = t3 - t1
 
 print
-print "Arvutuse võttis aega:", dt/60, "min"
+print "Arvutuse võttis kokku aega:", dtk/60, "min"
+print "import ja generation:", dt1/60, "min"
+print "generate westgaard", dtk/60, "min"
